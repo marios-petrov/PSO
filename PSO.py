2a55bd3d364f708417f44ee086de8d714b597488
Marios Petrov <118226559+marios-petrov@users.noreply.github.com>
2023-06-21 19:13:29 -0600

    Update PSO.py

--- PSO.py
+++ PSO.py
@@ -2,49 +2,59 @@ import numpy as np
 from tqdm import tqdm
 
 class Particle:
-    def __init__(self, lb, ub):
-        self.position = np.random.randint(low=lb, high=ub)
-        self.velocity = np.random.rand()
-        self.best_position = self.position
-        self.best_score = float('inf')
+    def __init__(self, dimensions):
+        self.position = np.random.choice([0, 1], size=(dimensions,), p=[0.5, 0.5])
+        self.prob_position = self.position.astype(float)  # This is the "probabilistic" position
+        self.velocity = np.random.rand(dimensions)
+        self.pbest_position = self.position
+        self.pbest_prob_position = self.prob_position
+        self.pbest_value = float('inf')
+
 
 class PSO:
-    def __init__(self, n_particles, dimensions, c1, c2, w_max, w_min, lb, ub, objective_function):
+    def __init__(self, n_particles, dimensions, c1, c2, w_max, w_min, iterations, objective_function):
         self.n_particles = n_particles
         self.dimensions = dimensions
         self.c1 = c1
         self.c2 = c2
         self.w_max = w_max
         self.w_min = w_min
-        self.lb = lb
-        self.ub = ub
-        self.particles = [Particle(lb, ub) for _ in range(n_particles)]
-        self.global_best_position = self.particles[0].position
-        self.global_best_score = float('inf')
+        self.iterations = iterations
         self.objective_function = objective_function
+        self.particles = [Particle(dimensions) for _ in range(n_particles)]
+        self.global_best_value = float('inf')
+        self.global_best_position = np.zeros((dimensions,))
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def update_velocity(self, particle, iteration):
+        w = self.w_max - iteration * ((self.w_max - self.w_min) / self.iterations)
+        r1 = np.random.uniform(size=self.dimensions)
+        r2 = np.random.uniform(size=self.dimensions)
+        cognitive_velocity = self.c1 * r1 * (particle.pbest_prob_position - particle.prob_position)
+        social_velocity = self.c2 * r2 * (self.global_best_position - particle.prob_position)
+        particle.velocity = w * particle.velocity + cognitive_velocity + social_velocity
+
+    def update_position(self, particle):
+        particle.prob_position = 1 / (1 + np.exp(-particle.velocity))
+        particle.position = particle.prob_position > np.random.uniform(size=self.dimensions)
 
     def run(self, iterations):
         for iteration in tqdm(range(iterations)):
             for particle in self.particles:
                 score = self.objective_function(particle.position)
-                if score < particle.best_score:
-                    particle.best_score = score
-                    particle.best_position = particle.position
-                if score < self.global_best_score:
-                    self.global_best_score = score
-                    self.global_best_position = particle.position
+                if score < particle.pbest_value:
+                    particle.pbest_value = score
+                    particle.pbest_position = particle.position
+                    particle.pbest_prob_position = particle.prob_position
+                if score < self.global_best_value:
+                    self.global_best_value = score
+                    self.global_best_position = particle.prob_position
 
-            w = self.w_max - iteration * ((self.w_max - self.w_min) / iterations)
             for particle in self.particles:
-                new_velocity = (w * particle.velocity) + (self.c1 * np.random.rand() * (particle.best_position - particle.position)) + (self.c2 * np.random.rand() * (self.global_best_position - particle.position))
-                new_position = particle.position + new_velocity
+                self.update_velocity(particle, iteration)
+                self.update_position(particle)
 
-                if new_position < self.lb:
-                    new_position = self.lb
-                elif new_position > self.ub:
-                    new_position = self.ub
-
-                particle.position = int(new_position)
-                particle.velocity = new_velocity
         return self.global_best_position
 
