3f52dd7c64c42eb02953588b6ec345268f91d2d2
Marios Petrov <118226559+marios-petrov@users.noreply.github.com>
2023-06-23 09:21:30 -0600

    Update PSO.py

--- PSO.py
+++ PSO.py
@@ -1,60 +1,61 @@
 import numpy as np
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from tqdm import tqdm
 
-class Particle:
-    def __init__(self, dimensions):
-        self.position = np.random.choice([0, 1], size=(dimensions,), p=[0.5, 0.5])
-        self.prob_position = self.position.astype(float)  # This is the "probabilistic" position
-        self.velocity = np.random.rand(dimensions)
-        self.pbest_position = self.position
-        self.pbest_prob_position = self.prob_position
-        self.pbest_value = float('inf')
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 
-class PSO:
-    def __init__(self, n_particles, dimensions, c1, c2, w_max, w_min, iterations, objective_function):
-        self.n_particles = n_particles
-        self.dimensions = dimensions
-        self.c1 = c1
-        self.c2 = c2
-        self.w_max = w_max
-        self.w_min = w_min
-        self.iterations = iterations
-        self.objective_function = objective_function
-        self.particles = [Particle(dimensions) for _ in range(n_particles)]
-        self.global_best_value = float('inf')
-        self.global_best_position = np.zeros((dimensions,))
+def PSO(X, y, n_particles, n_iterations, w_max, w_min, c1, c2):
+    dim = X.shape[1]
 
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
+    # Initialize particles
+    particles = np.random.uniform(low=-1, high=1, size=(n_particles, dim))
+    velocities = np.random.uniform(low=-1, high=1, size=(n_particles, dim))
 
-    def update_velocity(self, particle, iteration):
-        w = self.w_max - iteration * ((self.w_max - self.w_min) / self.iterations)
-        r1 = np.random.uniform(size=self.dimensions)
-        r2 = np.random.uniform(size=self.dimensions)
-        cognitive_velocity = self.c1 * r1 * (particle.pbest_prob_position - particle.prob_position)
-        social_velocity = self.c2 * r2 * (self.global_best_position - particle.prob_position)
-        particle.velocity = w * particle.velocity + cognitive_velocity + social_velocity
+    pbest_positions = particles.copy()
+    pbest_scores = np.full(shape=(n_particles,), fill_value=float('inf'))
+    gbest_position = None
+    gbest_score = float('inf')
 
-    def update_position(self, particle):
-        particle.prob_position = 1 / (1 + np.exp(-particle.velocity))
-        particle.position = particle.prob_position > np.random.uniform(size=self.dimensions)
+    for iteration in tqdm(range(n_iterations), desc='PSO progress'):
+        w = w_max - (w_max - w_min) * iteration / n_iterations
 
-    def run(self, iterations):
-        for iteration in tqdm(range(iterations)):
-            for particle in self.particles:
-                score = self.objective_function(particle.position)
-                if score < particle.pbest_value:
-                    particle.pbest_value = score
-                    particle.pbest_position = particle.position
-                    particle.pbest_prob_position = particle.prob_position
-                if score < self.global_best_value:
-                    self.global_best_value = score
-                    self.global_best_position = particle.prob_position
+        # Binary mask
+        masks = sigmoid(particles) > 0.5
 
-            for particle in self.particles:
-                self.update_velocity(particle, iteration)
-                self.update_position(particle)
+        for i, mask in enumerate(masks):
+            # Skip if all features are masked out
+            if np.sum(mask) == 0:
+                continue
 
-        return self.global_best_position
+            X_train, X_test, y_train, y_test = train_test_split(X[:, mask], y, test_size=0.2, random_state=42)
 
+            # Train SVM
+            clf = svm.SVC()
+            clf.fit(X_train, y_train)
+
+            # Evaluate accuracy
+            y_pred = clf.predict(X_test)
+            accuracy = accuracy_score(y_test, y_pred)
+
+            # Update personal best
+            if accuracy < pbest_scores[i]:
+                pbest_positions[i] = particles[i]
+                pbest_scores[i] = accuracy
+
+            # Update global best
+            if accuracy < gbest_score:
+                gbest_position = particles[i]
+                gbest_score = accuracy
+
+        # Update velocity and position
+        for i in range(n_particles):
+            velocities[i] = w * velocities[i] + c1 * np.random.uniform() * (
+                        pbest_positions[i] - particles[i]) + c2 * np.random.uniform() * (gbest_position - particles[i])
+            particles[i] += velocities[i]
+
+    return gbest_position, gbest_score
